dfd6e073efd6c06708256f9bbac4de43e696a6c8
gallus-gallus <anyllmarkevich@gmail.com>
2024-07-11 01:11:50 -0600

    Testing github...

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use ecolysis_rs::*;
 fn main() {
     println!("Welcome to Ecolysis RS, a Rust tool for ecologicial simulation and analysis.");
     ecolysis_rs::test();
-    // Testing git.
+    // Testing git. 2
 }
