8155683d926a9d0c3d7487604da2c38b5e1797c9
Anyll Markevich <anyllmarkevich@gmail.com>
2024-07-24 12:37:47 -0600

    Matrix projection (multiplying a vector by a matrix to get a new population) is now a function implemented as part of a population matrix as opposed to its own function.

--- src/populations/population_level_simulation.rs
+++ src/populations/population_level_simulation.rs
@@ -57,63 +57,65 @@ impl PopulationMatrix {
     pub fn get_matrix(&self) -> &Vec<LifestageSurvivalVector> {
         return &self.matrix;
     }
-}
 
-/// # Multiply a Population Matrix by a Population vector
-/// ## Contract
-/// Given an input of a PopulationMatrix and a PopulationVector with the same number of items
-/// in their `matrix` and `vector` values respectively, the function will return a
-/// PopulationMatrix.
-/// ##Use
-/// This function multiplies a vector by a matrix, but does so in the context of relevant types.
-/// This function takes a population matrix (formatted as a PopulationMatrix struct) and a population vector (formatted as a PopulationVector struct) as an imput, calculating the sum of the components of each LifeStageSurvivalVector within the population matrix and muliplying it by the value of the population vector at the same index. It then returns a new PopulationVector.
-///
-/// This can be visually thought of as such:
-///
-///[1]   [11,  2, 0.5 ]   [ 13.5]
-///[5] x [ 1, 10, 3   ] = [ 70  ]
-///[8]   [20,  6, 0.25]   [210  ]
-///
-///This calculation is common in Population Variability Analysis (PVA) wherein each row (LifeStageSurvivalVector) represents the probability of recruitment into that life stage over the course of a year. By multiplying a matrix of these probabilities by a vector containing the current population, a researcher can estimate the following year's population.
-///
-/// ## Errors
-/// This function will return an Err('static str') if the number of life stage items in the matrix is not equal to the number of items in the population vector.
-///
-/// Although this is theoretically impossible, the program could also panic if it recieves and out-of-bounds index request for the population vector. However, the function checks for this earlier in order to return a useful error code, so should neever occur.
-/// # Esamples
-/// ```
-/// use ecolysis_cmd::populations::population_level_simulation::{LifestageSurvivalVector,
-/// PopulationMatrix, popmatrix_by_popvector, PopulationVector};
-///
-/// let popvector = PopulationVector::new(vec![150.0, 200.0, 33.0]);
-///
-/// let mut lifestage_recruit: Vec<LifestageSurvivalVector> = vec![LifestageSurvivalVector::new(vec![0.25, 0.001, 0.75])];
-/// lifestage_recruit.push(LifestageSurvivalVector::new(vec![0.3, 0.4346, 0.002]));
-/// lifestage_recruit.push(LifestageSurvivalVector::new(vec![0.98, 0.66, 0.161]));
-///
-/// let popmatrix = PopulationMatrix::build(lifestage_recruit).unwrap();
-///
-/// let new_popvector = popmatrix_by_popvector(&popmatrix, &popvector);
+    /// # Multiply a Population Matrix by a Population vector
+    /// ## Contract
+    /// Given an input of a PopulationMatrix and a PopulationVector with the same number of items
+    /// in their `matrix` and `vector` values respectively, the function will return a
+    /// PopulationMatrix.
+    /// ##Use
+    /// This function multiplies a vector by a matrix, but does so in the context of relevant types.
+    /// This function takes a population matrix (formatted as a PopulationMatrix struct) and a population vector (formatted as a PopulationVector struct) as an imput, calculating the sum of the components of each LifeStageSurvivalVector within the population matrix and muliplying it by the value of the population vector at the same index. It then returns a new PopulationVector.
+    ///
+    /// This can be visually thought of as such:
+    ///
+    ///[1]   [11,  2, 0.5 ]   [ 13.5]
+    ///[5] x [ 1, 10, 3   ] = [ 70  ]
+    ///[8]   [20,  6, 0.25]   [210  ]
+    ///
+    ///This calculation is common in Population Variability Analysis (PVA) wherein each row (LifeStageSurvivalVector) represents the probability of recruitment into that life stage over the course of a year. By multiplying a matrix of these probabilities by a vector containing the current population, a researcher can estimate the following year's population.
+    ///
+    /// ## Errors
+    /// This function will return an Err('static str') if the number of life stage items in the matrix is not equal to the number of items in the population vector.
+    ///
+    /// Although this is theoretically impossible, the program could also panic if it recieves and out-of-bounds index request for the population vector. However, the function checks for this earlier in order to return a useful error code, so should never occur.
+    /// # Examples
+    /// ```
+    /// use ecolysis_cmd::populations::population_level_simulation::{LifestageSurvivalVector,
+    /// PopulationMatrix, PopulationVector};
+    ///
+    /// let popvector = PopulationVector::new(vec![150.0, 200.0, 33.0]);
+    ///
+    /// let mut lifestage_recruit: Vec<LifestageSurvivalVector> = vec![LifestageSurvivalVector::new(vec![0.25, 0.001, 0.75])];
+    /// lifestage_recruit.push(LifestageSurvivalVector::new(vec![0.3, 0.4346, 0.002]));
+    /// lifestage_recruit.push(LifestageSurvivalVector::new(vec![0.98, 0.66, 0.161]));
+    ///
+    /// let popmatrix = PopulationMatrix::build(lifestage_recruit).unwrap();
+    ///
+    /// let new_popvector = popmatrix.project_vector(&popvector);
 
-/// println!("{:?}", new_popvector.unwrap().get_vector());
+    /// println!("{:?}", new_popvector.unwrap().get_vector());
 
-/// ```
-pub fn popmatrix_by_popvector(
-    matrix: &PopulationMatrix,
-    vector: &PopulationVector,
-) -> Result<PopulationVector, &'static str> {
-    if matrix.get_matrix().len() != vector.get_vector().len() {
-        return Err("the length of inputted population matrix and population vector do not match.");
+    /// ```
+    pub fn project_vector(
+        &self,
+        vector: &PopulationVector,
+    ) -> Result<PopulationVector, &'static str> {
+        if self.matrix.len() != vector.get_vector().len() {
+            return Err(
+                "the length of inputted population matrix and population vector do not match.",
+            );
+        }
+        let mut new_population_vector: Vec<f64> = vec![];
+        for (count, lifestage) in self.matrix.iter().enumerate() {
+            let total: f64 = lifestage.get_vector().iter().sum();
+            new_population_vector
+                .push(total * vector.get_value_at_index(count as u32)
+                .expect("Unexpected Error: the length of inputted population matrix and population vector do not match."));
+            // This .expect should never panic. The earlier check for matching vector lengths should ensure this.
+        }
+        Ok(PopulationVector::new(new_population_vector))
     }
-    let mut new_population_vector: Vec<f64> = vec![];
-    for (count, lifestage) in matrix.get_matrix().iter().enumerate() {
-        let total: f64 = lifestage.get_vector().iter().sum();
-        new_population_vector
-            .push(total * vector.get_value_at_index(count as u32)
-            .expect("Unexpected Error: the length of inputted population matrix and population vector do not match."));
-        // This .expect should never panic. The earlier check for matching vector lengths should ensure this.
-    }
-    Ok(PopulationVector::new(new_population_vector))
 }
 
 pub fn PVA_simple_projection_matrix(
@@ -124,7 +126,7 @@ pub fn PVA_simple_projection_matrix(
     let mut active_vector = initial_population.clone();
     let mut result: Vec<PopulationVector> = Vec::new();
     for _ in 1..=iterations {
-        active_vector = popmatrix_by_popvector(&projection_matrix, &active_vector)?;
+        active_vector = projection_matrix.project_vector(&active_vector)?;
         result.push(active_vector.clone());
     }
     return Ok(result);
@@ -146,7 +148,8 @@ mod tests {
         let result: Vec<f64> = vec![1537.65, 20486.92, 6171.0];
         assert_eq!(
             &result,
-            &popmatrix_by_popvector(&popmatrix, &popvector)
+            &popmatrix
+                .project_vector(&popvector)
                 .unwrap()
                 .get_vector()
                 .into_iter()
